61dfb15ff836b75fd7649b3067e10ee163909835
Charles-DevFr <charlesleul@gmail.com>
2024-03-21 16:39:50 +0100

    Commit

--- Javascript05.js
+++ Javascript05.js
@@ -1,1 +1,31 @@
-var 
+// Exercice 1
+
+/*x = prompt ("Entrez un chiffre");
+y = prompt ("Entrez un autre chiffre")
+
+function produit (x , y) {
+    let resultat = x * y;
+    document.getElementById("produit").innerHTML = " Le produit de " +x+ " x " +y+ " est égal à " +resultat
+    return resultat;
+}
+function carre (x) {
+    let resultatcube = x * x * x;
+    document.getElementById("produitcube").innerHTML = " Le cube de " +x+ " est égal à " +resultatcube
+    return resultatcube;
+}
+
+
+resultat = produit(x , y)
+console.log (resultat);
+resultatcube = carre(x , x)
+console.log (resultatcube);
+
+function afficheImage (image) {
+    const imgElement = document.getElementById("img");
+    imgElement.src = "minion.jpg";
+}
+afficheImage () /*
+
+//Exercice 2
+
+
